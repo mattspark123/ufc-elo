9fcf31939df1bed347ad6bbbe3b584327902c21f
mattspark123 <mattspark123@gmail.com>
2025-04-23 17:42:13 -0500

    adding predictions for Prates vs Garry

--- predict_next_event.py
+++ predict_next_event.py
@@ -36,24 +36,38 @@ for row in fightTable.find_all('tr')[2:]:
 
 df = pd.read_csv('data\\elo_list_output.csv')
 
-for i,row in enumerate(fighterArray):
-	print('Fight',i+1)
-	if df.loc[df['Name'].str.title() == row[0].split('(')[0].strip().title(), 'Elo'].empty: 
-		print(f'{row[0].split('(')[0].strip()} is making their debut in the UFC')
-		fighter1Elo=1200
-	else:
-		fighter1Elo = df.loc[df['Name'].str.title() == row[0].split('(')[0].strip().title(), 'Elo'].iloc[0]
+with open('data\\next_event_predictions.txt', 'w') as f_out:
+    for i, row in enumerate(fighterArray):
+        f_out.write(f'Fight {i+1}\n')
+        print(f'Fight {i+1}')
 
-	if df.loc[df['Name'].str.title() == row[1].split('(')[0].strip().title(), 'Elo'].empty: 
-		print(f'{row[1].split('(')[0].strip()} is making their debut in the UFC')
-		fighter2Elo=1200
-	else:
-		fighter2Elo = df.loc[df['Name'].str.title() == row[1].split('(')[0].strip().title(), 'Elo'].iloc[0]
+        fighter1_name = row[0].split('(')[0].strip().title()
+        fighter2_name = row[1].split('(')[0].strip().title()
 
-	probList = elo.eloProb(fighter1Elo,fighter2Elo)
-	fighter1Odds = elo.calcOdds(probList[0])
-	fighter2Odds = elo.calcOdds(probList[1])
-	adjOdds = elo.adjustVig(fighter1Odds,fighter2Odds)
+        if df.loc[df['Name'].str.title() == fighter1_name, 'Elo'].empty: 
+            debut_msg1 = f'{fighter1_name} is making their debut in the UFC\n'
+            print(debut_msg1.strip())
+            f_out.write(debut_msg1)
+            fighter1Elo = 1200
+        else:
+            fighter1Elo = df.loc[df['Name'].str.title() == fighter1_name, 'Elo'].iloc[0]
 
-	print(f'{row[0]} Wins: {probList[0]}% / {adjOdds[0]}\n{row[1]} Wins: {probList[1]}%  / {adjOdds[1]}')
-	print('\n')
+        if df.loc[df['Name'].str.title() == fighter2_name, 'Elo'].empty: 
+            debut_msg2 = f'{fighter2_name} is making their debut in the UFC\n'
+            print(debut_msg2.strip())
+            f_out.write(debut_msg2)
+            fighter2Elo = 1200
+        else:
+            fighter2Elo = df.loc[df['Name'].str.title() == fighter2_name, 'Elo'].iloc[0]
+
+        probList = elo.eloProb(fighter1Elo, fighter2Elo)
+        fighter1Odds = elo.calcOdds(probList[0])
+        fighter2Odds = elo.calcOdds(probList[1])
+        adjOdds = elo.adjustVig(fighter1Odds, fighter2Odds)
+
+        result_text = (
+            f'{row[0]} Wins: {probList[0]}% / {adjOdds[0]}\n'
+            f'{row[1]} Wins: {probList[1]}% / {adjOdds[1]}\n\n'
+        )
+        print(result_text.strip())
+        f_out.write(result_text)
